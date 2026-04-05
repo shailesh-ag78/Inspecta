7a8f193d76bd4d191a5574204595d9e06a5beb56
shailesh-ag78 <shailesh.ag78@gmail.com>
2026-04-05 23:42:27 +0530

    Made changes after formatting

--- FieldReporterAgent/src/main.py
+++ FieldReporterAgent/src/main.py
@@ -33,7 +33,7 @@ MODEL_TEMPERATURE = float(os.getenv("MODEL_TEMPERATURE", "0.2"))
 logger.info(f"🚀 Starting Executor with {env_path} ENV_MODE={ENV_MODE}   {MODEL} (temp={MODEL_TEMPERATURE})")
 
 # Define your local root (where files actually live on your PC)
-LOCAL_STORAGE_ROOT = os.path.abspath(os.getenv("LOCAL_STORAGE_ROOT", r"d:\code\Inspecta\Data"))
+LOCAL_STORAGE_ROOT = os.path.abspath(os.getenv("LOCAL_STORAGE_ROOT", r"G:\code\Inspecta\Data"))
 LOCAL_TEMP_FOLDER = os.path.join(LOCAL_STORAGE_ROOT, "temp")
 if not os.path.exists(LOCAL_TEMP_FOLDER):
     os.makedirs(LOCAL_TEMP_FOLDER)
